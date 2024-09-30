473d8c06e74685f6c76fba6f6d3ffc94ab947c40
Stakrotsky <Stakrotsky@gmail.com>
2024-09-30 10:27:07 +0300

    init app

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setTaskText, setEditTaskText, setEditTask } from './redux/actions/uiActions';
+import { Tasks } from './components/Tasks';
+import styles from './app.module.css';
+
+export const App = () => {
+	// Подключаем состояния из соответствующих редьюсеров
+	const { taskText, editTaskId, editTaskText } = useSelector((state) => state.ui);
+	const { isSorted, error, isLoading } = useSelector((state) => state.filters);
+
+	const dispatch = useDispatch();
+
+	// Подключаем обработчики из Tasks.js
+	const {
+		handleAddTask,
+		handleSaveEditedTask,
+		handleDeleteTask,
+		handleSort,
+		handleSearchChange,
+		handleEditTask,
+		sortedTasks, // Подключаем отсортированные и отфильтрованные задачи
+	} = Tasks();
+
+	return (
+		<div className={styles.app}>
+			<h3 className={styles.title}>Список задач:</h3>
+			<div className={styles['input-block']}>
+				<input
+					type="text"
+					value={taskText}
+					onChange={(e) => dispatch(setTaskText(e.target.value))}
+					placeholder="Ввести новую задачу"
+				/>
+				<button onClick={handleAddTask}>Добавить задачу</button>
+			</div>
+			{error && <div className={styles['error-block']}>{error}</div>}
+			<div className={styles['search-and-sort-block']}>
+				<input
+					type="text"
+					onChange={handleSearchChange}
+					placeholder="Поиск задач"
+				/>
+				<button onClick={handleSort}>
+					{isSorted ? 'Отмена сортировки' : 'Сортировать по алфавиту'}
+				</button>
+			</div>
+			<div className={styles['tasks-block']}>
+				<h3 className={styles.title}>Текущие задачи:</h3>
+				{isLoading ? (
+					<div className={styles.loader}></div>
+				) : (
+					<ul className={styles['tasks-block-list']}>
+						{sortedTasks.map((task) => (
+							<li key={task.id} className={styles['tasks-block-item']}>
+								{editTaskId === task.id ? (
+									<div className={styles['task-item-content']}>
+										<input
+											type="text"
+											value={editTaskText}
+											onChange={(e) =>
+												dispatch(setEditTaskText(e.target.value))
+											}
+											className={styles['task-input']}
+										/>
+										<div className={styles['task-item-buttons']}>
+											<button
+												onClick={handleSaveEditedTask}
+												className={styles['save-button']}
+											>
+												Сохранить
+											</button>
+											<button
+												onClick={() =>
+													dispatch(setEditTask(null, ''))
+												}
+												className={styles['cancel-save-button']}
+											>
+												Отмена
+											</button>
+										</div>
+									</div>
+								) : (
+									<div className={styles['task-item-content']}>
+										<span>{task.text}</span>
+										<div className={styles['task-item-buttons']}>
+											<button onClick={() => handleEditTask(task)}>
+												Изменить
+											</button>
+											<button
+												onClick={() => handleDeleteTask(task.id)}
+												className={styles['delete-button']}
+											>
+												Удалить
+											</button>
+										</div>
+									</div>
+								)}
+							</li>
+						))}
+					</ul>
+				)}
+			</div>
+		</div>
+	);
+};
---
+++ src/components/Tasks.js
@@ -0,0 +1,67 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTasks, addTask, editTask, deleteTask } from '../redux/actions/tasksAction';
+import { setEditTask, setTaskText } from '../redux/actions/uiActions';
+import { setSearchQuery, setSorted } from '../redux/actions/filterActions';
+
+export const Tasks = (props) => {
+	const dispatch = useDispatch();
+
+	const { taskText, editTaskId, editTaskText } = useSelector((state) => state.ui);
+	const { tasks } = useSelector((state) => state.tasks);
+	const { searchQuery, isSorted } = useSelector((state) => state.filters);
+
+	useEffect(() => {
+		dispatch(fetchTasks());
+	}, [dispatch]);
+
+	const handleAddTask = () => {
+		if (taskText.trim()) {
+			dispatch(addTask(taskText)).then(() => {
+				dispatch(setTaskText(''));
+			});
+		}
+	};
+
+	const handleEditTask = (task) => {
+		dispatch(setEditTask(task.id, task.text));
+	};
+
+	const handleSaveEditedTask = () => {
+		if (editTaskText.trim()) {
+			dispatch(editTask(editTaskId, editTaskText)).then(() => {
+				dispatch(setEditTask(null, ''));
+			});
+		}
+	};
+
+	const handleDeleteTask = (id) => {
+		dispatch(deleteTask(id));
+	};
+
+	const handleSort = () => {
+		dispatch(setSorted(!isSorted));
+	};
+
+	const handleSearchChange = (e) => {
+		dispatch(setSearchQuery(e.target.value));
+	};
+
+	const filteredTasks = tasks.filter((task) =>
+		task.text.toLowerCase().includes(searchQuery.toLowerCase()),
+	);
+
+	const sortedTasks = isSorted
+		? [...filteredTasks].sort((a, b) => a.text.localeCompare(b.text))
+		: filteredTasks;
+
+	return {
+		handleAddTask,
+		handleSaveEditedTask,
+		handleDeleteTask,
+		handleSort,
+		handleSearchChange,
+		handleEditTask,
+		sortedTasks,
+	};
+};
